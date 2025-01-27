591eb6a87e835ac7efbffc975e154f03f7c97552
oiboiiii <toma.ttora@gmail.com>
2025-01-27 11:21:36 +0300

    adds pie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import { Chart } from "react-google-charts";
+
+
 function App() {
+  const data = [
+    ["Тип", "часов в день"],
+    ["Работа", 11],
+    ["Еда", 2],
+    ["Чуханье кота", 2],
+    ["Отдых", 2],
+    ["Сон", 7],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+    pieHole: 0.4, // Creates a Donut Chart. Does not do anything when is3D is enabled
+    is3D: true, // Enables 3D view
+    // slices: {
+    //   1: { offset: 0.2 }, // Explodes the second slice
+    // },
+    pieStartAngle: 100, // Rotates the chart
+    sliceVisibilityThreshold: 0.02, // Hides slices smaller than 2%
+    legend: {
+      position: "bottom",
+      alignment: "center",
+      textStyle: {
+        color: "#233238",
+        fontSize: 14,
+      },
+    },
+    colors: ["#8AD1C2", "#9F8AD1", "#D18A99", "#BCD18A", "#D1C28A"],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"600px"}
+    />
   );
 }
 
